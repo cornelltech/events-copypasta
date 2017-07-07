dafbd610480bf19accd40e47be7017753884dbc4
Rachel Sobel <rachel.sobel@cornell.edu>
2017-07-07 16:18:24 -0400

    add end times and locations, create new locations if they don't exist yet

--- build_contentful_data.py
+++ build_contentful_data.py
@@ -1,6 +1,9 @@
+import contentful
+import contentful_management
 import os
 import pdb
 
+from contentful import Client
 from contentful_management import Client
 from os.path import join, dirname
 from dotenv import load_dotenv
@@ -14,6 +17,28 @@ except Exception as e:
 # contentful space data
 SPACE_ID = os.environ.get('SPACE_ID', None)
 MGMT_TOKEN = os.environ.get('MANAGEMENT_TOKEN', None)
+ACCESS_TOKEN = os.environ.get('ACCESS_TOKEN', None)
+
+def get_locations():
+    client = contentful.Client(SPACE_ID, ACCESS_TOKEN)
+    return client.entries({'content_type': 'location'}).items
+
+# creates new location if none exists yet
+def find_location_id(lat, lon, name, room):
+    locations = get_locations()
+    print 'lat=', lat, 'lon=', lon, 'name=', name,
+    # pdb.set_trace()
+    for location in locations:
+        if location.address.lat == lat:
+            if location.address.lon == lon:
+                if location.name == name:
+                    if location.room == room:
+                        return location.sys['id']
+    # if it doesn't exist, create it
+    new_location = build_location(lat, lon, name, room)
+    loc = send_to_contentful(new_location)
+    loc.publish()
+    return loc.sys['id']
 
 def build_field(name, contents):
     if contents:
@@ -21,12 +46,29 @@ def build_field(name, contents):
     else:
         return
 
-def build_event(title, startTime=None, description=None, external_url=None):
+# complicated formatting
+def build_location_link(location_id):
+    return {'en-US': [{'sys': {'linkType': 'Entry', 'type': 'Link', 'id': location_id}}]}
+
+def build_location(lat, lon, name, room):
+    fields_data = []
+    fields_data.append(build_field('name', name))
+    fields_data.append(build_field('room', room))
+    fields_data.append(build_field('address', {'lat': lat, 'lon': lon}))
+    fields = {}
+    for field in fields_data:
+        fields.update(field)
+    return {'content_type_id': 'location', 'fields': fields}
+
+def build_event(title, start_time=None, end_time=None, description=None,
+                external_url=None, location_id=None):
     fields_data = []
     fields_data.append(build_field('eventTitle', title))
-    fields_data.append(build_field('startTime', startTime))
+    fields_data.append(build_field('startTime', start_time))
+    fields_data.append(build_field('endTime', end_time))
     fields_data.append(build_field('description', description))
     fields_data.append(build_field('externalUrl', external_url))
+    fields_data.append({'locationObject': build_location_link(location_id)})
     fields = {}
     for field in fields_data:
         if field:
@@ -37,15 +79,21 @@ def build_tag(title):
     fields = build_field('title', title)
     return {'content_type_id': 'tag', 'fields': fields}
 
-def send_event_to_contentful(event_attributes):
-    client = Client(MGMT_TOKEN)
-    new_entry = client.entries(SPACE_ID).create(None, event_attributes)
-
+def send_to_contentful(attributes):
+    client = contentful_management.Client(MGMT_TOKEN)
+    new_entry = client.entries(SPACE_ID).create(None, attributes)
+    return new_entry
 
 if __name__ == '__main__':
+    # lat, lon, name, room
+    lat = 40.740914
+    lon = -74.00218100000001
+    name = 'The Foundry'
+    room = '#802'
     entry_attributes = build_event('party friday',
-                                    startTime='2017-06-27T19:00:00-04:00',
-                                    description='it is friday let\'s party!')
+                                    start_time='2017-06-27T19:00:00-04:00',
+                                    description='it is friday let\'s party!',
+                                    location_id=find_location_id(lat, lon, name, room))
 
-    # send_event_to_contentful(entry_attributes)
     print entry_attributes
+    send_to_contentful(entry_attributes)
